31d3026a48f1d1dea57745416f157db84f9af39f
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-01-17 11:36:18 +0700

    feat: create counter_bloc_class_screen.dart, counter_bloc_enum_screen.dart and counter_cubit_screen.dart

---
+++ lib/screen/counter_bloc_class_screen.dart
@@ -0,0 +1,65 @@
+import 'package:bloc_counter_app/counter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+// * Using Event Kind Class from bloc
+class CounterBlocClassScreen extends StatelessWidget {
+  const CounterBlocClassScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Implement Bloc Using Event(Class)'),
+      ),
+      body: Center(
+        child: BlocProvider(
+          create: (context) => CounterBloc(),
+          child: BlocBuilder<CounterBloc, CounterState>(
+            builder: (context, state) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "${state.numberValue}",
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Increment',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15.0,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Decrement',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
